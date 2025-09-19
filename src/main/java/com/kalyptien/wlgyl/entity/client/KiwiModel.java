a69cee2cca0f5730d1f1a4f85d835d9267f85f06
Kalyptien <louppingetpro@gmail.com>
2025-09-19 19:59:56 +0200

    Switch between sit and up ok BUT keep walking when sit

--- src/main/java/com/kalyptien/wlgyl/entity/client/KiwiModel.java
+++ src/main/java/com/kalyptien/wlgyl/entity/client/KiwiModel.java
@@ -18,35 +18,33 @@ public class KiwiModel<T extends KiwiNormalEntity> extends HierarchicalModel<T> 
     public static final ModelLayerLocation LAYER_LOCATION =
             new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(WhenLifeGivesYouLemonsMod.MOD_ID, "kiwi_normal"), "main");
 
-    private final ModelPart Body;
+    private final ModelPart Kiwi;
     private final ModelPart Head;
-    private final ModelPart LegL;
-    private final ModelPart LegR;
 
     public KiwiModel(ModelPart root) {
-        this.Body = root.getChild("Body");
-        this.Head = root.getChild("Head");
-        this.LegL = root.getChild("LegL");
-        this.LegR = root.getChild("LegR");
+        this.Kiwi = root.getChild("Kiwi");
+        this.Head = this.Kiwi.getChild("Head");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 12).addBox(-3.0F, -3.25F, 0.5F, 6.0F, 6.0F, 3.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-3.0F, -3.25F, 3.5F, 6.0F, 7.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 17.25F, -2.5F));
+        PartDefinition Kiwi = partdefinition.addOrReplaceChild("Kiwi", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(18, 20).addBox(-0.5F, -0.25F, -8.0F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(18, 12).addBox(-2.0F, -2.25F, -4.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 15.25F, 0.0F));
+        PartDefinition Body = Kiwi.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 12).addBox(-3.0F, -3.25F, 0.5F, 6.0F, 6.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-3.0F, -3.25F, 3.5F, 6.0F, 7.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -6.75F, -2.5F));
 
-        PartDefinition LegL = partdefinition.addOrReplaceChild("LegL", CubeListBuilder.create().texOffs(22, 0).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 3.0F, new CubeDeformation(0.0F))
+        PartDefinition Head = Kiwi.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(18, 20).addBox(-0.5F, -0.25F, -8.0F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F))
+                .texOffs(18, 12).addBox(-2.0F, -2.25F, -4.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -8.75F, 0.0F));
+
+        PartDefinition LegL = Kiwi.addOrReplaceChild("LegL", CubeListBuilder.create().texOffs(22, 0).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 3.0F, new CubeDeformation(0.0F))
                 .texOffs(22, 8).addBox(-0.5F, 1.0F, 0.5F, 1.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(10, 21).addBox(-0.5F, 3.0F, -2.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(1.75F, 21.0F, 3.0F));
+                .texOffs(10, 21).addBox(-0.5F, 3.0F, -2.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(1.75F, -3.0F, 3.0F));
 
-        PartDefinition LegR = partdefinition.addOrReplaceChild("LegR", CubeListBuilder.create().texOffs(22, 5).addBox(-0.5F, 3.0F, -2.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F))
+        PartDefinition LegR = Kiwi.addOrReplaceChild("LegR", CubeListBuilder.create().texOffs(22, 5).addBox(-0.5F, 3.0F, -2.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F))
                 .texOffs(22, 10).addBox(-0.5F, 1.0F, 0.5F, 1.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 21).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.75F, 21.0F, 3.0F));
+                .texOffs(0, 21).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.75F, -3.0F, 3.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
@@ -56,8 +54,8 @@ public class KiwiModel<T extends KiwiNormalEntity> extends HierarchicalModel<T> 
         this.root().getAllParts().forEach(ModelPart::resetPose);
         this.applyHeadRotation(netHeadYaw, headPitch);
 
-        this.animateWalk(KiwiAnimation.KIWI_NORMAL_WALK, limbSwing, limbSwingAmount, 2f, 2.5f);
-        this.animate(entity.sitAnimationState, KiwiAnimation.KIWI_NORMAL_SIT, ageInTicks, 0f);
+        this.animateWalk(KiwiAnimation.KIWI_NORMAL_WALK, limbSwing, limbSwingAmount, 1f, 2.5f);
+        this.animate(entity.sitAnimationState, KiwiAnimation.KIWI_NORMAL_SIT, ageInTicks, 1f);
     }
 
 
@@ -71,14 +69,11 @@ public class KiwiModel<T extends KiwiNormalEntity> extends HierarchicalModel<T> 
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, int color) {
-        Body.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-        Head.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-        LegL.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-        LegR.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
+        Kiwi.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
     }
 
     @Override
     public ModelPart root() {
-        return Body;
+        return Kiwi;
     }
 }
--- src/main/java/com/kalyptien/wlgyl/entity/custom/KiwiNormalEntity.java
+++ src/main/java/com/kalyptien/wlgyl/entity/custom/KiwiNormalEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -15,13 +16,19 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.registries.DeferredItem;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KiwiNormalEntity extends Animal {
 
+    private static final Logger log = LoggerFactory.getLogger(KiwiNormalEntity.class);
     public final AnimationState sitAnimationState = new AnimationState();
     private int sitAnimationTimeout = 0;
+    private int timeSitting = 0;
     public DeferredItem<Item> agrume = ModItems.LEMON;
 
     public KiwiNormalEntity(EntityType<? extends Animal> entityType, Level level) {
@@ -62,12 +69,50 @@ public class KiwiNormalEntity extends Animal {
     }
 
     private void setupAnimationStates() {
+
         if(this.sitAnimationTimeout <= 0) {
-            this.sitAnimationTimeout = 2200;
+            this.sitAnimationTimeout = 5000;
             this.sitAnimationState.start(this.tickCount);
+            this.timeSitting = 540;
+            this.setPose(Pose.SITTING);
+            this.gameEvent(GameEvent.ENTITY_ACTION);
         } else {
             --this.sitAnimationTimeout;
         }
+
+        if(!this.isSitting()){
+            this.timeSitting = 0;
+            this.sitAnimationState.stop();
+            this.setPose(Pose.STANDING);
+            this.gameEvent(GameEvent.ENTITY_ACTION);
+        }
+        else {
+            if (this.isSitting()) {
+                --this.timeSitting;
+            }
+        }
+
+        if (isSitting() && this.isInWater()) {
+            this.timeSitting = 0;
+        }
+
+        if (isSitting() && this.hurtTime > 0) {
+            this.timeSitting = 0;
+        }
+
+    }
+
+    public void travel(Vec3 travelVector) {
+        if (this.sitAnimationState.isStarted() && this.onGround()) {
+            this.setDeltaMovement(this.getDeltaMovement().multiply(0.0, 1.0, 0.0));
+            travelVector = travelVector.multiply(0.0, 1.0, 0.0);
+        }
+
+        super.travel(travelVector);
+    }
+
+    public boolean isSitting() {
+        return this.timeSitting > 0;
     }
 
     @Override
